981242a1c82b44ec650db6513cabb1e12da85f66
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-09-18 16:44:06 -0500

    Accepting name as an argument

--- python-latex-resume.py
+++ python-latex-resume.py
@@ -4,7 +4,6 @@ import argparse
 
 templates_dir = 'templates/'
 resumes_dir = 'resumes/'
-name = 'Kelly Kemnitz'
 
 # Create the parser
 parser = argparse.ArgumentParser(description='A Python script to convert a resume template in LaTeX format to a PDF, based on type of role.')
@@ -14,9 +13,19 @@ parser.add_argument(
     type=str,
     choices=['it_analyst', 'qa_engineer', 'qa_analyst', 'sdet', 'software_dev', 'sre'],
     default='qa_engineer',
-    help='Type of resume to generate')
+    help='Type of resume to generate'
+)
+
+parser.add_argument(
+    '-n', '--name',
+    type=str,
+    nargs='+',
+    required=True,
+    help='Name to use in filename'
+)
 
 args = parser.parse_args()
+full_name = ' ' .join(args.name)
 
 type_mapping = {
     'it_analyst': 'IT Analyst',
@@ -30,7 +39,7 @@ type_mapping = {
 try:
     pdfl = PDFLaTeX.from_texfile(os.path.join(templates_dir, f'{args.type}.tex'))
     pdfl.set_output_directory(resumes_dir)
-    pdf_filename = f'{name} - {type_mapping[args.type]}'
+    pdf_filename = f'{full_name} - {type_mapping[args.type]}'
     pdfl.set_pdf_filename(pdf_filename)
     pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=False)
     print(f'{pdf_filename}.pdf generated successfully and placed in {resumes_dir}')
